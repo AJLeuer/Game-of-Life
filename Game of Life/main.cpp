0e8318576e9864e6c322c19617ffbf9ad64fd3ff
Adam J. Leuer <aleuer2@illinois.edu>
2017-09-05 13:04:25 -0500

    Initial Commit

---
+++ Game of Life/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game of Life
+//
+//  Created by Adam J Leuer on 9/5/17.
+//  Copyright © 2017 ThoughtWorks, Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
